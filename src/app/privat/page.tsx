7552788ba25da3ce3070f16018372fd158c19b0d
HansG9 <131452363+HansG9@users.noreply.github.com>
2024-04-25 14:49:28 +0200

    Update page.tsx
    private page styling bug from last change fixed

--- src/app/privat/page.tsx
+++ src/app/privat/page.tsx
@@ -11,7 +11,7 @@ export default function PrivatPage() {
     
     return (
         <main>
-            <div className="h-full w-full">
+            <div className="h-full w-full min-h-screen">
                 <div className='flex flex-col p-10'>
                     {folders.map((file, index) => (
                         <Link key={index} href={"/privat/" + file}>
